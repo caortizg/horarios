0236802a935a823c35404c1380bbf0e49fa55c64
rfbrandon <Brandon Robles@DESKTOP-6H3RJUS>
2016-06-26 22:46:07 -0500

    Se agrega plugin para visualizar las actividades de los usuarios

--- horarios/var/cache/dev/twig/e0/e0e817203e717de7ec9c83a3f4143450e53761574d49ed5b59054957e575ac51.php
+++ horarios/var/cache/dev/twig/e0/e0e817203e717de7ec9c83a3f4143450e53761574d49ed5b59054957e575ac51.php
@@ -20,12 +20,12 @@ class __TwigTemplate_66189ae9649df470e04d8dbe85c9ddd079036679a5025a3e1582be01104
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_5180cc70a8c0ffa4bd0ebc4eedfa844823585c54f3b90d1e83b047abd410f5bb = $this->env->getExtension("native_profiler");
-        $__internal_5180cc70a8c0ffa4bd0ebc4eedfa844823585c54f3b90d1e83b047abd410f5bb->enter($__internal_5180cc70a8c0ffa4bd0ebc4eedfa844823585c54f3b90d1e83b047abd410f5bb_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "CaogHorariosBundle:Default:index.html.twig"));
+        $__internal_795f0dac9a044133a6d62a7dfd0d6a5cde0635b087dfb6ea614dcf24c66b2c61 = $this->env->getExtension("native_profiler");
+        $__internal_795f0dac9a044133a6d62a7dfd0d6a5cde0635b087dfb6ea614dcf24c66b2c61->enter($__internal_795f0dac9a044133a6d62a7dfd0d6a5cde0635b087dfb6ea614dcf24c66b2c61_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "CaogHorariosBundle:Default:index.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_5180cc70a8c0ffa4bd0ebc4eedfa844823585c54f3b90d1e83b047abd410f5bb->leave($__internal_5180cc70a8c0ffa4bd0ebc4eedfa844823585c54f3b90d1e83b047abd410f5bb_prof);
+        $__internal_795f0dac9a044133a6d62a7dfd0d6a5cde0635b087dfb6ea614dcf24c66b2c61->leave($__internal_795f0dac9a044133a6d62a7dfd0d6a5cde0635b087dfb6ea614dcf24c66b2c61_prof);
 
     }
 
